96e7224eca9913209b3070ba64ad472f3536600e
Abdul Basit <bassitb50@gmail.com>
2026-05-03 15:59:12 +0500

    Assignment Push

--- batch-05/Section 1 — SQL/Class01 - Querying Sorting Filtering/Assignment01 - Querying Sorting Filtering.sql
+++ batch-05/Section 1 — SQL/Class01 - Querying Sorting Filtering/Assignment01 - Querying Sorting Filtering.sql
@@ -14,7 +14,9 @@
 -- ============================================================
 
 -- Write your query below:
-
+SELECT first_name, last_name, city, phone
+FROM sales.customers
+WHERE state = 'CA' AND phone IS NOT NULL;
 
 
 
@@ -27,7 +29,9 @@
 -- ============================================================
 
 -- Write your query below:
-
+SELECT product_id, product_name, model_year, list_price
+FROM production.products
+ORDER BY model_year DESC, list_price ASC;
 
 
 
@@ -41,10 +45,15 @@
 -- ============================================================
 
 -- Part a:
-
+SELECT TOP 5 product_name, list_price
+FROM production.products;
 
 -- Part b:
+SELECT TOP 5 PERCENT *
+FROM production.products
+ORDER BY list_price ASC;
 
+SELECT COUNT(*) FROM production.products;
 
 
 
@@ -59,13 +68,22 @@
 -- ============================================================
 
 -- Page 1:
-
+SELECT *
+FROM production.products
+ORDER BY list_price DESC
+OFFSET 0 ROWS FETCH NEXT 10 ROWS ONLY;
 
 -- Page 2:
-
+SELECT *
+FROM production.products
+ORDER BY list_price DESC
+OFFSET 10 ROWS FETCH NEXT 10 ROWS ONLY;
 
 -- Page 3:
-
+SELECT *
+FROM production.products
+ORDER BY list_price DESC
+OFFSET 20 ROWS FETCH NEXT 10 ROWS ONLY;
 
 
 
@@ -81,13 +99,19 @@
 -- ============================================================
 
 -- Part a:
+SELECT DISTINCT state
+FROM sales.customers
+ORDER BY state ASC;
 
 
 -- Part b:
-
+SELECT DISTINCT state, city
+FROM sales.customers
+ORDER BY state ASC, city ASC;
 
 -- Part c:
-
+SELECT COUNT(DISTINCT model_year) AS unique_model_years
+FROM production.products;
 
 
 
@@ -103,3 +127,8 @@
 -- ============================================================
 
 -- Write your query below:
+SELECT product_id, product_name, brand_id, category_id, list_price
+FROM production.products
+WHERE list_price BETWEEN 500 AND 1500
+AND (model_year = 2019 OR model_year = 2020)
+ORDER BY list_price ASC;
